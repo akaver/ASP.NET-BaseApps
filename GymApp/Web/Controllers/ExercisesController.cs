d709bafa8569dcf4dc4d8af222dd339015f93ab2
Kaspar Tilk <ktilk@itcollege.ee>
2016-04-20 11:27:17 +0300

    Lisatud Exercise Controller, VM ja vaated - testimata!

--- GymApp/Web/Controllers/ExercisesController.cs
+++ GymApp/Web/Controllers/ExercisesController.cs
@@ -9,143 +9,134 @@ using System.Web.Mvc;
 using DAL;
 using DAL.Interfaces;
 using Domain;
-using Microsoft.AspNet.Identity;
 using PagedList;
 using Web.ViewModels;
-
 
 namespace Web.Controllers
 {
     [Authorize]
-    public class PersonsController : BaseController
+    public class ExercisesController : BaseController
     {
+        //private DataBaseContext db = new DataBaseContext();
         private readonly IUOW _uow;
 
-        public PersonsController(IUOW uow)
+        public ExercisesController(IUOW uow)
         {
             _uow = uow;
         }
 
-        // GET: Persons
-        public ActionResult Index(PersonIndexViewModel vm)
+        // GET: Exercises
+        public ActionResult Index(ExerciseIndexViewModel vm)
         {
-            //TODO kust totalUserCount ja realSortProperty väärtused saavad??
             int totalUserCount;
             string realSortProperty;
 
-            // if not set, set base values
+            //if not set, set base values
             vm.PageNumber = vm.PageNumber ?? 1;
             vm.PageSize = vm.PageSize ?? 25;
 
-            var res = _uow.Persons.GetAllForUser(User.Identity.GetUserId<int>(), vm.Filter, vm.SortProperty, vm.PageNumber.Value-1, vm.PageSize.Value, out totalUserCount, out realSortProperty);
+            var res = _uow.Exercises.All;
+            //TODO kust realSortProperty väärtuse saab?
+            //vm.SortProperty = realSortProperty;
 
-            vm.SortProperty = realSortProperty;
-
-            // https://github.com/kpi-ua/X.PagedList
-            vm.Persons = new StaticPagedList<Person>(res, vm.PageNumber.Value, vm.PageSize.Value, totalUserCount);
-
+            vm.Exercises = new StaticPagedList<Exercise>(res, vm.PageNumber.Value, vm.PageSize.Value, totalUserCount);
+            //var exercises = db.Exercises.Include(e => e.ExerciseType);
             return View(vm);
         }
 
-        // GET: Persons/Details/5
+        // GET: Exercises/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Person person = _uow.Persons.GetById(id);
-            if (person == null)
+            Exercise exercise = _uow.Exercises.GetById(id);
+            if (exercise == null)
             {
                 return HttpNotFound();
             }
-
-            return View(person);
+            return View(exercise);
         }
 
-        // GET: Persons/Create
+        // GET: Exercises/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Persons/Create
+        // POST: Exercises/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Person person)
+        public ActionResult Create([Bind(Include = "ExerciseId,ExerciseTypeId,ExerciseName,Description,Instructions,VideoUrl,Rating,DateCreated,CreatedAtDT,CreatedBy,ModifiedAtDT,ModifiedBy")] Exercise exercise)
         {
             if (ModelState.IsValid)
             {
-                // do not get user id from html get/post!!!!
-                person.UserId = User.Identity.GetUserId<int>();
-
-                _uow.Persons.Add(person);
+                _uow.Exercises.Add(exercise);
                 _uow.Commit();
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(person);
+            return View(exercise);
         }
 
-        // GET: Persons/Edit/5
+        // GET: Exercises/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            // check user id!!!!
-            Person person = _uow.Persons.GetForUser(id.Value, User.Identity.GetUserId<int>());
-            if (person == null)
+            Exercise exercise = _uow.Exercises.GetById(id);
+            if (exercise == null)
             {
                 return HttpNotFound();
             }
-
-            return View(person);
+            
+            return View(exercise);
         }
 
-        // POST: Persons/Edit/5
+        // POST: Exercises/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Person person)
+        public ActionResult Edit([Bind(Include = "ExerciseId,ExerciseTypeId,ExerciseName,Description,Instructions,VideoUrl,Rating,DateCreated,CreatedAtDT,CreatedBy,ModifiedAtDT,ModifiedBy")] Exercise exercise)
         {
             if (ModelState.IsValid)
             {
-                // do not get user id from html get/post!!!!
-                person.UserId = User.Identity.GetUserId<int>();
-
-                _uow.Persons.Update(person);
+                _uow.Exercises.Update(exercise);
                 _uow.Commit();
                 return RedirectToAction(nameof(Index));
             }
-            return View(person);
+            
+            return View(exercise);
         }
 
-        // GET: Persons/Delete/5
+        // GET: Exercises/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Person person = _uow.Persons.GetForUser(id.Value, User.Identity.GetUserId<int>());
-            if (person == null)
+            Exercise exercise = _uow.Exercises.GetById(id);
+            if (exercise == null)
             {
                 return HttpNotFound();
             }
-            return View(person);
+            return View(exercise);
         }
 
-        // POST: Persons/Delete/5
+        // POST: Exercises/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            _uow.Persons.Delete(id);
+            Exercise exercise = _uow.Exercises.GetById(id);
+            _uow.Exercises.Delete(id);
             _uow.Commit();
             return RedirectToAction(nameof(Index));
         }
@@ -159,4 +150,3 @@ namespace Web.Controllers
         }
     }
 }
-
