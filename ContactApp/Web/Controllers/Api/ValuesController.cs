11811b24a17c67470e379dac4a9d66439674bbbc
Andres Käver <akaver@akaver.com>
2016-04-22 11:09:49 +0300

    Web API ninject integration install-package Ninject.Web.WebApi install-package Ninject.Web.WebApi.WebHost

--- ContactApp/Web/Controllers/Api/ValuesController.cs
+++ ContactApp/Web/Controllers/Api/ValuesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Http;
+using DAL.Interfaces;
 using Microsoft.AspNet.Identity;
 
 namespace Web.Controllers.Api
@@ -8,11 +9,22 @@ namespace Web.Controllers.Api
     [Authorize]
     public class ValuesController : ApiController
     {
+        private readonly IUOW _uow;
+
+        // for Web API ninject integration!!!!
+        // install-package Ninject.Web.WebApi
+        // install-package Ninject.Web.WebApi.WebHost
+
+        public ValuesController(IUOW uow)
+        {
+            _uow = uow;
+        }
+
         // GET api/values
         public string Get()
         {
             var userName = User.Identity.GetUserName();
-            return $"Hello, {userName}."+DateTime.Now;
+            return $"Hello, {userName}. DT is "+DateTime.Now + " Persons in DB:"+_uow.Persons.All.Count;
         }
     }
 }
